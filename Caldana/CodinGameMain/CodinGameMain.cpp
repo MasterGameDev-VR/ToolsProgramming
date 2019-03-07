1d3af11b76358ae88f9e3be18989f002a6e86b6b
DESKTOP-0GA23AQ\ciald <riccardo.caldana01@gmail.com>
2019-03-07 19:00:07 +0100

    Third Commit

--- Caldana/CodinGameMain/CodinGameMain.cpp
+++ Caldana/CodinGameMain/CodinGameMain.cpp
@@ -4,10 +4,37 @@
 #include "pch.h"
 #include <iostream>
 #include "..\NumberOfLettersInANumber\NumberOfLettersInANumber.h"
+#include "..\NetworkCabling\NetworkCabling.h"
+
+using namespace std;
 
 int main()
 {
-	NumberOfLettersInANumberNS::NumberOfLettersInANumber instance("..\\Test\\NumberOfLettersInANumber_01.txt");
-	instance.Execute();
+	int choice;
+	cout << "Riccardo Caldana - Master CGD 2018/2019 - Tools Programming Exam\n";
+	cout << "Insert 1 to select NumberOfLetterInANumber\nInsert 2 to select NetworkCabling\n";
+	cin >> choice;
+	switch (choice) 
+	{
+	case 1:
+	{
+		NumberOfLettersInANumberNS::NumberOfLettersInANumber instance("..\\Test\\NumberOfLettersInANumber_01.txt");
+		instance.Execute();
+		break;
+	}
+	case 2:
+	{
+		NetworkCablingNS::NetworkCabling instance1("..\\Test\\NetworkCabling_01.txt");
+		instance1.Execute();
+		break;
+	}
+	default:
+	{
+		cout << "Wrong selection - Exiting";
+		break;
+	}
+	}
+	
+
 }
 
