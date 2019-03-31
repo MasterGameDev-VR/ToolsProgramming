68076103dc2490cf08425aa5895e1153ea0ec470
nov4 <andrea.novati@gmail.com>
2019-03-31 16:27:18 +0200

    Fixed minor issues, changed cin to input, added mayancalculation to codingamemain

--- Novati/CodinGameMain.cpp
+++ Novati/CodinGameMain.cpp
@@ -1,7 +1,9 @@
 #include "pch.h"
 #include <iostream>
 #include "AsciiArtStringTest.h"
+#include "MayanCalculationTest.h"
 #include "GameOfLifeLibrary.h"
+
 #include <windows.h>
 
 using namespace std;
@@ -9,10 +11,10 @@ using namespace CodinGame;
 
 typedef int(__stdcall *f_funci)();
 
-typedef GAMEOFLIFE_API void(*ConstructTestFunction)(const string INPUT_FILENAME);
+typedef GAMEOFLIFE_API void (*ConstructTestFunction)(const string INPUT_FILENAME);
 //typedef void(*ConstructTestFunction)(const string INPUT_FILENAME);
-typedef GAMEOFLIFE_API void(*ExecuteTestFunction)();
-typedef GAMEOFLIFE_API void(*ReleaseTestFunction)();
+typedef GAMEOFLIFE_API void (*ExecuteTestFunction)();
+typedef GAMEOFLIFE_API void (*ReleaseTestFunction)();
 
 	int main()
 	{
@@ -30,13 +32,15 @@ typedef GAMEOFLIFE_API void(*ReleaseTestFunction)();
 		cout << "1) AsciiArt (easy)\n";
 		cout << "2) GameOfLife (medium)\n";
 		cout << "3) Gravity (medium)\n";
-		cout << "4) Quit\n\n";
+		cout << "4 NEW!) Mayan Calculation(medium)\n";
+		cout << "5) Quit\n\n";
 
 		int test_selected = 0;
 		while (!exit) {
 			cout << "Choose Project: ";
 			cin >> project;
 			AsciiArtStringTest *test = NULL;
+			MayanCalculationTest *m_test = NULL;
 
 			switch (project) {
 			case 1: {
@@ -252,6 +256,95 @@ typedef GAMEOFLIFE_API void(*ReleaseTestFunction)();
 				break;
 			}
 			case 4: {
+				cout << "Choose a test case [1-12]: ";
+				cin >> test_case;
+				switch (test_case) {
+				case 1: {
+					m_test = new MayanCalculationTest("MayanCalculation01.txt");
+					test_selected = 2;
+
+					break;
+				}
+				case 2: {
+					m_test = new MayanCalculationTest("MayanCalculation02.txt");
+					test_selected = 2;
+
+					break;
+				}
+				case 3: {
+					m_test = new MayanCalculationTest("MayanCalculation03.txt");
+					test_selected = 2;
+
+					break;
+				}
+				case 4: {
+					m_test = new MayanCalculationTest("MayanCalculation04.txt");
+					test_selected = 2;
+
+					break;
+				}
+				case 5: {
+					m_test = new MayanCalculationTest("MayanCalculation05.txt");
+					test_selected = 2;
+
+					break;
+				}
+				case 6: {
+					m_test = new MayanCalculationTest("MayanCalculation06.txt");
+					test_selected = 2;
+
+					break;
+				}
+				case 7: {
+					m_test = new MayanCalculationTest("MayanCalculation07.txt");
+					test_selected = 2;
+
+					break;
+				}
+				case 8: {
+					m_test = new MayanCalculationTest("MayanCalculation08.txt");
+					test_selected = 2;
+
+					break;
+				}
+				case 9: {
+					m_test = new MayanCalculationTest("MayanCalculation09.txt");
+					test_selected = 2;
+
+					break;
+				}
+				case 10: {
+					m_test = new MayanCalculationTest("MayanCalculation10.txt");
+					test_selected = 2;
+
+					break;
+				}
+				case 11: {
+					m_test = new MayanCalculationTest("MayanCalculation11.txt");
+					test_selected = 2;
+
+					break;
+				}
+				case 12: {
+					m_test = new MayanCalculationTest("MayanCalculation12.txt");
+					test_selected = 2;
+
+					break;
+				}
+				default: {
+					cout << "\nYOUR ARGUMENT HAS NEVER BEEN SO INVALID";
+					break;
+				}
+				};
+
+				if (test_selected == 1)
+					test->Execute();
+
+				if (test_selected == 2)
+					m_test->Execute();
+				break;
+			}
+			case 5: {
 				cout << "\nQUIT";
 				exit = 1;
 				break;
@@ -261,8 +354,6 @@ typedef GAMEOFLIFE_API void(*ReleaseTestFunction)();
 				break;
 			}
 			};
-
-			
 		}
 
 		return 0;
