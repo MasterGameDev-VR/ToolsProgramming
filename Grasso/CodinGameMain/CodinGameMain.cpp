9baa972ac38b4b77c2b8564c86af21599e2d78de
mtt-gra <45692824+mtt-gra@users.noreply.github.com>
2019-10-28 00:34:45 +0100

    Fourth commit

--- Grasso/CodinGameMain/CodinGameMain.cpp
+++ Grasso/CodinGameMain/CodinGameMain.cpp
@@ -6,7 +6,13 @@
 struct Exercise
 {
 	std::string m_name;
-	std::string m_testFile;
+
+	std::string m_testsBaseDir;
+	std::string m_testsBaseFile;
+	std::string m_testsExtension;
+
+	short int m_maxTests;
+
 	void (*m_run)(std::string);
 };
 
@@ -21,8 +27,8 @@ int main()
 {
 	const int MAX_EXERCISES = 2;
 	Exercise exercises[MAX_EXERCISES];
-	exercises[0] = { "The Gift", "..\\TheGift\\TheGift.txt", &RunExercise<TheGift> };
-	exercises[1] = { "War", "..\\War\\War.txt", &RunExercise<War> };
+	exercises[0] = { "The Gift", "..\\TheGift\\Tests", "TheGift_", "txt", 9, &RunExercise<TheGift> };
+	exercises[1] = { "War", "..\\War\\Tests", "War_", "txt", 9, &RunExercise<War> };
 
 	int action;
 	do {
@@ -45,18 +51,39 @@ int main()
 		std::cout << "Inserisci il nr. esercizio: ";
 		std::cin >> action;
 
-		if (action > 0 && action <= MAX_EXERCISES) {
-			std::cout << "\nSoluzione:" << std::endl;
+		if (action > 0 && action <= MAX_EXERCISES)
+		{
+			const int exerciseId = action - 1;
+			Exercise& exercise = exercises[exerciseId];
+			
+			short int testId;
+			std::cout << "Inserisci il nr. test [1 - " << exercise.m_maxTests << "]: ";
+			std::cin >> testId;
 
-			const int id = action - 1;
-			Exercise& exercise = exercises[id];
+			if (testId > 0 && testId <= exercise.m_maxTests)
+			{
+				std::cout << "\nSoluzione:" << std::endl;
 
-			exercise.m_run(exercise.m_testFile);
+				std::string filename = exercise.m_testsBaseDir;
+				filename.append("\\");
+				filename.append(exercise.m_testsBaseFile);
+				filename.append(std::to_string(testId));
+				filename.append(".");
+				filename.append(exercise.m_testsExtension);
+
+				exercise.m_run(filename);
+			}
+			else
+			{
+				std::cout << "Valore non valido" << std::endl;
+			}
 		}
-		else if (action != 0) {
+		else if (action != 0)
+		{
 			std::cout << "Valore non valido" << std::endl;
 		}
-		else {
+		else
+		{
 			break;
 		}
 
