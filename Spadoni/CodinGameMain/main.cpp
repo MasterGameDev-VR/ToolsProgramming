f399c984331224c11d6516a848ee0d977cd4c267
lucaspadoni <lucaspado@hotmail.it>
2020-05-01 15:34:35 +0200

    Automatic tests

--- Spadoni/CodinGameMain/main.cpp
+++ Spadoni/CodinGameMain/main.cpp
@@ -12,26 +12,18 @@ int main()
 	tests.push_back("Data//TheDescent_1.txt");
 	tests.push_back("Data//TheDescent_2.txt");
 
-	std::cout << "Inserire il numero di test da eseguire: " << std::endl;
-	unsigned int testIndex;
-	std::cin >> testIndex;
-
-	if (testIndex > tests.size())
-	{
-		std::cout << "Test not found!" << std::endl;
-		return -1;
-	}
-
 	try 
 	{
-		if (testIndex == 0) {
-			IGame* game = new SkynetRevolution(tests[testIndex]);
-			game->Execute();
-		}
-		else
-		{
-			IGame* game = new TheDescent(tests[testIndex]);
-			game->Execute();
+		for (int testIndex = 0; testIndex < tests.size(); testIndex++) {
+			if (testIndex == 0) {
+				IGame* game = new SkynetRevolution(tests[testIndex]);
+				game->Execute();
+			}
+			else
+			{
+				IGame* game = new TheDescent(tests[testIndex]);
+				game->Execute();
+			}
 		}
 	}
 	catch (std::runtime_error error)
